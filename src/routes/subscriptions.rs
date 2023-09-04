654d14c4afdd69488af29aadc0a19dadbb9364d7
Austin Perrine <akperrine@gmail.com>
2023-09-03 20:49:31 -0700

    update query fmt

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,8 +13,8 @@ pub struct FormData {
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     match sqlx::query!(
         r#"
-        INSERT INTO subscriptions (id, email, name, subscribed_at)
-        VALUES ($1, $2, $3, $4)
+    INSERT INTO subscriptions (id, email, name, subscribed_at)
+    VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
         form.email,
