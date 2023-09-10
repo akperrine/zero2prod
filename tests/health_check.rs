fa64bf7e05efeade77a9bae1f7404f31e14ab9fa
Austin Perrine <akperrine@gmail.com>
2023-09-10 08:00:16 -0700

    add actix-web tacing

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,9 +1,11 @@
 use once_cell::sync::Lazy;
-use sqlx::PgPool;
+use secrecy::ExposeSecret;
+use sqlx::{PgPool, PgConnection, Connection, Executor};
 use std::net::TcpListener;
-use tracing::subscriber;
-use zero2prod::configuration::get_configuration;
+use uuid::Uuid;
+use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
+use zero2prod::startup::run;
 
 static TRACING: Lazy<()> = Lazy::new(|| {
     let default_filter_level = "info".to_string();
@@ -25,29 +27,48 @@ pub struct TestApp {
 
 // Spins up an isntance of the application and returns the address
 async fn spawn_app() -> TestApp {
-    // first time `initialize` is invoked the code in `TRACING` is executed.
-    // All other invocations will instead skip execution
+    // The first time `initialize` is invoked the code in `TRACING` is executed.
+    // All other invocations will instead skip execution.
     Lazy::force(&TRACING);
 
-    // Binds the listener to a random port
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    // Get the port assigned and start up a server instance
+    // We retrieve the port assigned to us by the OS
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
 
-    let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres");
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+    let connection_pool = configure_database(&configuration.database).await;
 
-    let server =
-        zero2prod::startup::run(listener, connection_pool.clone()).expect("Failed to bind address");
+    let server = run(listener, connection_pool.clone()).expect("Failed to bind address");
     let _ = tokio::spawn(server);
-
     TestApp {
         address,
         db_pool: connection_pool,
     }
+}
+
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    // Create database
+    let mut connection =
+        PgConnection::connect(config.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres");
+    connection
+        .execute(&*format!(r#"CREATE DATABASE "{}";"#, config.database_name))
+        .await
+        .expect("Failed to create database.");
+
+    // Migrate database
+    let connection_pool = PgPool::connect(config.connection_string().expose_secret())
+        .await
+        .expect("Failed to connect to Postgres.");
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database");
+
+    connection_pool
 }
 
 #[tokio::test]
